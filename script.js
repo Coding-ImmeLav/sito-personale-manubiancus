ec0b2beab5a6f9cfcd95cb6b1862f477659108b4
manubiancus <149148113+manubiancus@users.noreply.github.com>
2025-03-20 11:06:47 +0100

    menu e footer

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = 0;
+function comparimenu () {
+    if (a == 0) {
+        document.getElementById("menu").style.display = "initial";
+        a = 1;
+    }
+    else if(a == 1) {
+       document.getElementById("menu").style.display = "none"; 
+       a = 0;
+    }
+}
+
+
